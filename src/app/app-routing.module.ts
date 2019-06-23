609abcbe484cf3410fe5ecdfef4972e9d0ce8e42
Eric Lee <ranye0719@gmail.com>
2019-06-23 18:33:15 +0800

    reinit environment

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component'
+
+const routes: Routes = [
+  { path: 'main', component: MainComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
