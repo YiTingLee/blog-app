7a932a9ac812c2a76467aad72310c9186f44e698
Eric Lee <ranye0719@gmail.com>
2019-07-13 19:09:47 +0800

    updated remote location

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { NgModule } from '@angular/core';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const uri = 'http://localhost:3000/';
+const uri = `http://${location.hostname}:3000/`;
 export function createApollo(httpLink: HttpLink) {
   return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache()
   };
 }
 
@@ -17,8 +17,8 @@ export function createApollo(httpLink: HttpLink) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
-    },
-  ],
+      deps: [HttpLink]
+    }
+  ]
 })
 export class GraphQLModule {}
